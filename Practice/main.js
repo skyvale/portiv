e9cddb014f58741a86c66112ce8296226dd9fd1f
Skylar <skylar_valerio@yahoo.com>
2020-03-09 14:02:03 -0400

    Added wireframes

---
+++ Practice/main.js
@@ -0,0 +1,23 @@
+console.log("within js file");
+
+/*
+for (let i=1; i < 100; i++) {
+    if(i%3 === 0 && i%5 === 0) {
+        console.log("FizzBuzz");
+    } else if (i%5 === 0) {
+        console.log("Buzz");
+    } else if (i%3 === 0) {
+        console.log("Fizz");
+    } else {
+        console.log(i);
+    }
+} */
+
+/*
+let triangle = "";
+for (let i = 1; i < 7; i++) {
+    triangle += "#";
+    console.log(triangle);
+}
+*/
+
